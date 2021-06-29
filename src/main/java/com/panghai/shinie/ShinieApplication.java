d8364f679e8e6ac49a2e578cdf2d2eaab5207281
阿海 <oncwnuP9j-MqdFla7hxbpaC9K6NU@git.weixin.qq.com>
2021-06-29 16:18:21 +0800

    创建springboot框架

---
+++ src/main/java/com/panghai/shinie/ShinieApplication.java
@@ -0,0 +1,13 @@
+package com.panghai.shinie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShinieApplication.class, args);
+    }
+
+}
